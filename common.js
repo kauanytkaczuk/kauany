b14ea368848ca8e06361d918126383e5c9c9e464
kauanytkaczuk <110197259+kauanytkaczuk@users.noreply.github.com>
2024-10-24 16:17:55 -0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,13 @@
+informacoesGlobais.js
+const getCSS = (variavel) => {
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
